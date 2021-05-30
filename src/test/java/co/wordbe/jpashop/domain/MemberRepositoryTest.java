490d38bb2dd4703251227a884f27c35e9e0b1752
Wordbe <seonghojin3@gmail.com>
2021-05-30 20:48:10 +0900

    feat: database, jpa, logging 설정

--- src/test/java/co/wordbe/jpashop/domain/MemberRepositoryTest.java
+++ src/test/java/co/wordbe/jpashop/domain/MemberRepositoryTest.java
@@ -29,5 +29,6 @@ class MemberRepositoryTest {
         //then
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); // true: 같은 트랜잭션 안에서는 같은 영속성 컨텍스트이다. 같은 영속성 컨텍스트 안에서 1차캐시에 저장되므로 아이디가 같다면 같은 객체다
     }
 }
