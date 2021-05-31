7515bc2beef3a8ab3fcebb232def748070ae55ac
Wordbe <seonghojin3@gmail.com>
2021-05-31 22:02:34 +0900

    feat: domain 엔티티 생성, 매핑

---
+++ src/main/java/co/wordbe/jpashop/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package co.wordbe.jpashop.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/co/wordbe/jpashop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package co.wordbe.jpashop.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
