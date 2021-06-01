cd773200b6ef7112eaebcebe88716e8212f35701
Wordbe <seonghojin3@gmail.com>
2021-06-02 01:30:50 +0900

    feat: 상품 등록, 조회

---
+++ src/main/java/co/wordbe/jpashop/web/controller/ItemController.java
@@ -0,0 +1,45 @@
+package co.wordbe.jpashop.web.controller;
+
+import co.wordbe.jpashop.domain.item.Book;
+import co.wordbe.jpashop.domain.item.Item;
+import co.wordbe.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getName());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
