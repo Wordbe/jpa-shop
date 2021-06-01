1f96bcf1258f94c6e9031615bac43f179263e34b
Wordbe <seonghojin3@gmail.com>
2021-06-02 00:40:17 +0900

    feat: home resource

--- src/main/java/co/wordbe/jpashop/web/controller/HomeController.java
+++ src/main/java/co/wordbe/jpashop/web/controller/HomeController.java
@@ -1,9 +1,11 @@
-package co.wordbe.jpashop.web;
+package co.wordbe.jpashop.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
@@ -11,5 +13,11 @@ public class HomeController {
     public String hello(Model model) {
         model.addAttribute("data", "사과");
         return "hello";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
     }
 }
