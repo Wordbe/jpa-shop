0cc785afb947821622fd0012078b3a64ad7c8a79
Seongho Jin <seonghojin3gmail.com>
2021-06-16 21:32:17 +0900

    feat: Order 조회 엔티티 직접 노출

---
+++ src/main/java/co/wordbe/jpashop/api/OrderApiController.java
@@ -0,0 +1,65 @@
+package co.wordbe.jpashop.api;
+
+import co.wordbe.jpashop.domain.Address;
+import co.wordbe.jpashop.domain.Order;
+import co.wordbe.jpashop.domain.OrderItem;
+import co.wordbe.jpashop.domain.OrderStatus;
+import co.wordbe.jpashop.repository.OrderRepository;
+import co.wordbe.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByJPQL(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByJPQL(new OrderSearch());
+
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems();
+        }
+    }
+}
