4ff91fdaaedc064d1facf71b5891ea34c8ce1914
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-09-26 00:05:32 +0900

    feat: IDENTITY, SEQUENCE, TABLE, AUTO 전략

--- src/test/java/co/wordbe/jpashop/domain/badge/BadgeTest.java
+++ src/test/java/co/wordbe/jpashop/domain/badge/BadgeTest.java
@@ -3,7 +3,6 @@ package co.wordbe.jpashop.domain.badge;
 import co.wordbe.jpashop.repository.BadgeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
